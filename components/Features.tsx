41a9e6dbfd83a2ffa4cdd50a160204c6c7bdfbd1
Selorm <ranselorm@outlook.com>
2023-10-17 21:50:02 +0100

    Add GetApp section

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import Image from "next/image";
+import { FEATURES } from "@/constants";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="flexCenter flex-col overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24 md:px-[100px] h-full">
+      <h2 className="lg:text-[30px] text-[20px] bold-52  mb-[40px]">
+        Our Features
+      </h2>
+      <div className="w-full flex flex-col lg:flex-row items-center lg:items-start">
+        <div className="relative w-[440px] h-[800px] -mt-[50px] hidden xl:flex">
+          <Image src="/phone.png" alt="phone" fill className="object-contain" />
+        </div>
+        <div className="w-full">
+          <div className="flex flex-wrap justify-between items-center ml-[50px]">
+            {FEATURES.map((feature, index) => (
+              <div
+                className="xl:max-w-[350px] w-full flex flex-col gap-y-4 mb-10"
+                key={index}
+              >
+                <div
+                  className={`w-[80px] h-[80px] rounded-full bg-green-50 flex items-center justify-center`}
+                >
+                  <Image src={feature.icon} alt="icon" width={35} height={35} />
+                </div>
+                <h2 className="lg:text-[30px] text-[20px] font-bold capitalize leading-9">
+                  {feature.title}
+                </h2>
+                <p className="text-gray-30">{feature.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import Image from "next/image";
+import Button from "./Button";
 const GetApp = () => {
-  return <div></div>;
+  return (
+    <section className="h-[90vh] bg-pattern bg-cover bg-center bg-no-repeat ">
+      <div className="flex items-center h-full text-white overflow-hidden">
+        <div className="w-1/2 pl-10 flex flex-col gap-y-10">
+          <h2 className="text-[60px] leading-[60px] font-bold w-1/2">
+            Get for free now!
+          </h2>
+          <p className="regular-18">Available on iOS and Android</p>
+          <div className="flex items-center gap-10">
+            <Button
+              type="button"
+              title="App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+            />
+            <Button
+              type="button"
+              title="Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green"
+            />
+          </div>
+        </div>
+        <div className="w-1/2 flex justify-center">
+          <Image src="/phones.png" alt="Phones" width={500} height={1000} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetApp;
