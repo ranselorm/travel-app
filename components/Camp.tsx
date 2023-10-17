2287b599c82e01096fd9a817bc6db5aa57f56ff7
Selorm <ranselorm@outlook.com>
2023-10-17 17:34:54 +0100

    Add Guide section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -16,7 +16,7 @@ const Campsite = ({
 }: CampProps) => {
   return (
     <div
-      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat rounded-r-5xl 2xl:rounded-5xl`}
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-5xl rounded-r-5xl`}
     >
       <div className="flex flex-col h-full items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
@@ -24,22 +24,23 @@ const Campsite = ({
             <Image src="folded-map.svg" width={28} height={28} alt="map" />
           </div>
           <div className="text-white">
-            <h4 className="bold-18">{subtitle}</h4>
+            <h4 className="bold-18">{title}</h4>
             <p className="regular-16">{subtitle}</p>
           </div>
         </div>
         <div className="flexCenter gap-6">
-          <span className="flex -space-x-4 overflow-hidden">
+          <div className="flex -space-x-4 overflow-hidden">
             {PEOPLE_URL.map((url) => (
-              <Image
-                src={url}
-                alt="persons"
-                width={52}
-                height={52}
-                className="inline-block h-10 w-10 rounded-full"
-              />
+              // <Image
+              //   src={url}
+              //   alt="persons"
+              //   width={52}
+              //   height={52}
+              //   className="inline-block h-10 w-10 rounded-full"
+              // />
+              <p>h</p>
             ))}
-          </span>
+          </div>
           <span className="bold-18 text-white">{peopleJoined}</span>
         </div>
       </div>
@@ -49,7 +50,7 @@ const Campsite = ({
 
 const Camp = () => {
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20 lg:mb-10">
+    <section className="2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20 lg:mb-10 lg:px-10 ">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px] overflow-x-auto">
         <Campsite
           backgroundImage="bg-bg-img-1"
--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,61 @@
+import Image from "next/image";
 import React from "react";
 
 const Guide = () => {
-  return <div>Guide</div>;
+  return (
+    <section className="flexCenter flex-col">
+      <div className="max-container padding-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="text-green-50">WE ARE HERE FOR YOU</p>
+        <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center">
+          <h2 className="text-[50px] bold-52 lg:bold-88 my-5 lg:mb-0 xl:max-w-[550px]">
+            Guide You to Easy Path
+          </h2>
+          <p className="xl:max-w-[500px]">
+            Only with the hilink application you will no longer get lost and get
+            lost again, because we already support offline maps when there is no
+            internet connection in the field. Invite your friends, relatives and
+            friends to have fun in the wilderness through the valley and reach
+            the top of the mountain
+          </p>
+        </div>
+      </div>
+
+      <div className="relative flexCenter max-container w-full lg:px-8 ">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center lg:rounded-5xl"
+        />
+        <div className="absolute bg-white flex py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src={"/meter.svg"}
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flex flex-col gap-y-16">
+            <div className="flex flex-col">
+              <div className="flex">
+                <p className="text-gray-20 mr-10">Destination</p>
+                <p className="text-green-50 font-bold">48 mins</p>
+              </div>
+              <p className="bold-20">Aguas Calientes</p>
+            </div>
+            <div className="flex flex-col">
+              <div>
+                <p className="text-gray-20 mr-10">Start Track</p>
+              </div>
+              <p className="bold-20">Wonorejo Pasuruan</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Guide;
